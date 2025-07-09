401f2c014cf4e797cafbb55ece0a9e1977046b70
aliaksandr-pilipuk <115932811+pilipuk-aliaksandr@users.noreply.github.com>
2025-07-09 18:29:57 +0300

    init (#2)
    * init
    
    * debugged some problems with gradle
    
    ---------
    
    Co-authored-by: Uladzislau.Kaliaha <uladzislau.kaliaha@noogadev.com>

---
+++ hotels-api/src/main/java/by/pilipuk/HotelsApiApp.java
@@ -0,0 +1,11 @@
+package by.pilipuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelsApiApp {
+    public static void main(String[] args) {
+        SpringApplication.run(HotelsApiApp.class, args);
+    }
+}
---
+++ users-api/src/main/java/by/pilipuk/UsersApiApp.java
@@ -0,0 +1,11 @@
+package by.pilipuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersApiApp {
+    public static void main(String[] args) {
+        SpringApplication.run(UsersApiApp.class, args);
+    }
+}
